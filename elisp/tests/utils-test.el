cd233f783fad27c3b26bb9bc1e983cf604725e20
Marc Soda <marc@soda.fm>
2025-02-02 14:52:12 -0500

    first attempt at a test gen macro

--- elisp/tests/utils-test.el
+++ elisp/tests/utils-test.el
@@ -2,24 +2,34 @@
 
 (require 'excursion)
 
-(ert-deftest excursion--parse-filename-test ()
-  (let ((cases
-         `((("/foo") nil)
-           (("/excursion:/foo") nil)
-           (("/excursion:electron:/foo") ,'("excursion" "electron" "/foo"))
-           (("/excursion::/foo") ,'("excursion" "" "/foo"))
-           (("/excursion:electron:") ,'("excursion" "electron" ""))
-           (("/excursion:electron:/" ,'method) "excursion")
-           (("/excursion:electron:/" ,'host) "electron")
-           (("/excursion:electron:/" ,'file) "/")
-           (("/excursion:electron:/" ,'d) nil))))
-    (dolist (case cases)
-      (let* ((args (car case))
-             (file (car args))
-             (part (cadr args))
-             (expected (cadr case)))
-        (should
-         (equal (excursion--parse-filename file part) expected))))))
+(defmacro gen-test2 (fn-name args result &optional iteration)
+  "Create a test that takes 2 arguments."
+  (let ((arg1 (car args))
+        (arg2 (cadr args))
+        (test-name (intern (format
+                            "%s-%s-test"
+                            (symbol-name fn-name)
+                            iteration))))
+    `(ert-deftest ,test-name ()
+       (should
+        (equal (,fn-name ,arg1 ,arg2) ,result)))))
+
+(let* ((cases
+        '((("/foo") nil)
+          (("/excursion:/foo") nil)
+          (("/excursion:electron:/foo") '("excursion" "electron" "/foo"))
+          (("/excursion::/foo") '("excursion" "" "/foo"))
+          (("/excursion:electron:") '("excursion" "electron" ""))
+          (("/excursion:electron:/" 'method) "excursion")
+          (("/excursion:electron:/" 'host) "electron")
+          (("/excursion:electron:/" 'file) "/")
+          (("/excursion:electron:/" 'd) nil)))
+       (i 1))
+  (dolist (case cases)
+    (let ((args (car case))
+          (result (cadr case)))
+      (eval `(gen-test2 excursion--parse-filename ,args ,result ,i)))
+    (setq i (1+ i))))
 
 (ert-deftest excursion--file-p-test ()
   (let ((cases
